69e88df43308967c45d938136db772651cbc2b5d
James-a-Ferreira <james@ignitesynergy.com>
2022-06-24 00:50:08 -0600

    added CI

--- src/App.js
+++ src/App.js
@@ -43,8 +43,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        
+      {/* <header className="App-header">  
       </header> */}
 
       <section>
